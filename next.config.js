e4422bf06275b922f2f5d6298a19f75fe1da4e18
UlepChie <138436978+UlepChie@users.noreply.github.com>
2025-02-06 00:40:30 +0800

    Update deployment configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  distDir: 'dist',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
